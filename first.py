293647128d4cf05287037841220179fe22122363
superlv123 <15151824064@163.com>
2019-05-12 20:02:13 +0800

    Initial commit

---
+++ first.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding=utf-8 -*-
+# 本脚由小网工-小布编辑，用于乾颐盾Python课程！
+# 小布QQ：1172090974
+# 码云：https://gitee.com/superlv-tmt/events
+# 一起在网络的道路上协同进步
+
+def qytprint(x):
+    print(x)
+    print(x)
+if __name__ == '__main__':
+    qytprint('superlv')
---
+++ second.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+# 本脚由小网工-小布编辑，用于乾颐盾Python课程！
+# 小布QQ：1172090974
+# 码云：https://gitee.com/superlv-tmt/events
+# 一起在网络的道路上协同进步
+
+
+if __name__ == '__main__':
+    pass
